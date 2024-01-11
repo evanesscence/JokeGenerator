a08f71af6ec156952327909bd24ca09ec44d8527
evanesscence <102433438+evanesscence@users.noreply.github.com>
2024-01-11 09:49:30 +0300

    Initial Commit

---
+++ JokeGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JokeGenerator
+//
+//  Created by Karina ❦ on 11.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
