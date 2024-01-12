fbd37510aa0ad8d78f87343fa3f52e56a8c53a87
evanesscence <102433438+evanesscence@users.noreply.github.com>
2024-01-12 18:31:22 +0300

    view elements

--- JokeGenerator/ViewController.swift
+++ JokeGenerator/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: IBOutlets
+    @IBOutlet private var jokeId: UIStackView!
+    @IBOutlet private var jokeType: UIStackView!
+    @IBOutlet private var jokeSetup: UIStackView!
+    @IBOutlet private var refreshButton: UIButton!
+    @IBOutlet private var showButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    private func setViews(views: [UIView]) {
+        views.forEach {
+            $0.layer.cornerRadius = 8
+            $0.layer.borderWidth = 2
+            $0.layer.borderColor = UIColor.rBlack.cgColor
+        }
+    }
 }
 
